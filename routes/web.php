85cc4b9bf4621f395c2c929c8423c8dabdf7d3a1
TEMBELY-creator <hamadounbadountembely@gmail.com>
2025-06-24 09:27:05 +0000

    Livewire ok

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Etudiants;
+use App\Livewire\ContactezNous;
 
-use Livewire\Livewire;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
+// Route d'accueil
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+// Routes Livewire
+Route::prefix('')->group(function () {
+    Route::get('/etudiants', Etudiants::class)->name('etudiants');
+    Route::get('/contact', ContactezNous::class)->name('contact');
 });
 
-Route::get('/etudiants', \App\Livewire\Etudiants::class)->name('etudiants');
-
-Route::get('/contact', \App\Livewire\ContactezNous::class)->name('contact');
+// Authentification (si utilisée)
+// Auth::routes();
